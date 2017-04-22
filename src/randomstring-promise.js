8ffa6d3ecaba317ce61c4464b50b7dc8a9bf9524
yuichiroTCY <t.yic.yt@gmail.com>
2017-04-23 04:52:05 +0900

    Fix

--- src/randomstring-promise.js
+++ src/randomstring-promise.js
@@ -1,10 +1,13 @@
 const windowCrypto = typeof window !== 'undefined' && (window.crypto || window.msCrypto);
 
 let randomBytes;
-try {
-  randomBytes = require('crypto').randomBytes;
-} catch(e) {
-  randomBytes = null;
+if (!windowCrypto) {
+  try {
+    const crypto = require('crypto');
+    randomBytes = crypto && crypto.randomBytes;
+  } catch(e) {
+    randomBytes = null;
+  }
 }
 
 import { getCharset } from './charset';
@@ -17,11 +20,10 @@ export default function (length = 32, charset = 'alphanumeric') {
   const maxByte = 256 - (256 % charsLen);
   let targetLength = length;
 
-  return new Promise((resolve, reject) => {
-    function addRandomString(currentString) {
-      let string = currentString;
-
-      if (windowCrypto) {
+  if (windowCrypto) {
+    let string = '';
+    return new Promise((resolve, reject) => {
+      while (targetLength > 0) {
         // window.crypto.getRandomValues() is used if available (browser env)
         const size = Math.min(Math.ceil(targetLength * 256 / maxByte), 65535);
         const buf = new Uint8Array(size);
@@ -34,34 +36,36 @@ export default function (length = 32, charset = 'alphanumeric') {
             --targetLength;
           }
         }
+      }
+
+      resolve(string);
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    function addRandomString(currentString) {
+      let string = currentString;
+
+      // crypto.randomBytes() is used in nodejs
+      const size = Math.ceil(targetLength * 256 / maxByte);
+
+      randomBytes(size, (err, buf) => {
+        if (err) reject(err);
+
+        for (let i = 0; i < buf.length && targetLength > 0; ++i) {
+          const randomByte = buf.readUInt8(i);
+          if (randomByte < maxByte) {
+            string += chars.charAt(randomByte % charsLen);
+            --targetLength;
+          }
+        }
 
         if (targetLength > 0) {
           addRandomString(string);
         } else {
           resolve(string);
         }
-      } else {
-        // crypto.randomBytes() is used in nodejs
-        const size = Math.ceil(targetLength * 256 / maxByte);
-
-        randomBytes(size, (err, buf) => {
-          if (err) reject(err);
-
-          for (let i = 0; i < buf.length && targetLength > 0; ++i) {
-            const randomByte = buf.readUInt8(i);
-            if (randomByte < maxByte) {
-              string += chars.charAt(randomByte % charsLen);
-              --targetLength;
-            }
-          }
-
-          if (targetLength > 0) {
-            addRandomString(string);
-          } else {
-            resolve(string);
-          }
-        });
-      }
+      });
     }
 
     addRandomString('');
